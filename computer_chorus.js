0f200ad945ed9ef19858acf1c13a19b93ad4eda2
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2017-10-14 20:48:46 -0400

    semi-working visuals

--- computer_chorus.js
+++ computer_chorus.js
@@ -14,7 +14,8 @@
 var soloSinger = new p5.Speech();
 var chorusSingers = new p5.Speech();
 var chorusCreated = false;
-// var sungText = [];
+var sungText = [];
+var textSepatation = 40;
 
 // DOM variables
 var inputText, speakButton, listButton, singerButton, labels;
@@ -54,30 +55,42 @@ function setup(){
 	w = width+86;
 	dx = (TWO_PI / period) * xspacing;
 	singerTheta = 0.02;
+	singerAmplitude = 20;
 	singerValues = new Array(floor(w/xspacing));
 	chorusTheta = 0.02;
+	chorusAmplitude = 20;
 	chorusValues = new Array(floor(w/xspacing));
+
+	// start and end funcitons
+	soloSinger.onStart = exciteSinger();
+	soloSinger.onEnd = calmSinger();
+	chorusSingers.onStart = exciteChorus();
+	chorusSingers.onEnd   = calmChorus();
 
 	// first words
 	soloSinger.setPitch(random(0.2, 0.6));
 	soloSinger.setRate(random(1.1, 2.1));
+	sungText.push(inputText.value());
 	soloSinger.speak(inputText.value());
 
-	// sungText.push(inputText.value());
-	// printText();
 }
 
 function draw(){
 	background(272, 65, 90);
-	// calcWave();
-	// renderWave();
-	ellipse(width/2, height/2, 100, 100);
+	calcWave();
+	renderWave();
+	// ellipse(width/2, height/2, 100, 100);
+	printText();
 }
 
 function doSpeak(){
-	soloSinger.speak(inputText.value()); // debug printer for voice options
+	sungText.push(inputText.value());
+	soloSinger.speak(inputText.value());
+
+	// sing duet if it was created
 	if(chorusCreated){
 		let response = Math.floor(random(sexyWords.length));
+		sungText.push(sexyWords[response]);
 		chorusSingers.speak(sexyWords[response]);
 	}
 }
@@ -92,11 +105,14 @@ function createChorus(){
 	chorusCreated = true;
 }
 
-// function printText(){
-// 	for (let j = 0; j < text.length; j++) {
-// 		sungText[j];
-// 	}
-// }
+function printText(){
+	fill(255);
+	let  k = 0;
+	for (let j = sungText.length -1; j >= 0; j--) {
+		k++;
+		text(sungText[j], 600, textSepatation*k);
+	}
+}
 
 function calcWave() {
   // initializations
@@ -110,6 +126,7 @@ function calcWave() {
     singerValues[i] = sin(x)*singerAmplitude;
     x+=dx;
   }
+	singerTheta -= 0.01;
 
 	// variables reset
 	theta = 0;
@@ -119,17 +136,38 @@ function calcWave() {
     chorusValues[i] = sin(x)*chorusAmplitude;
     x-=dx;
   }
+	chorusTheta += 0.01;
+
 }
 function renderWave() {
   noStroke();
+
   // Draw singer wave
-	fill(134,100,88);
+	fill(272+10,50,100);
 	for (let i = 0; i < singerValues.length; i++) {
-    ellipse(i*xspacing, height/2+singerValues[i], 12, 12);
+    ellipse(i*xspacing, height*.7 +singerValues[i], 12, 12);
   }
+
 	// Draw chorus wave
-	fill(234,55,100);
+	fill(272-10,100,50);
 	for (let i = 0; i < chorusValues.length; i++) {
-		ellipse(i*xspacing, height/2+chorusValues[i], 12, 12);
+		ellipse(i*xspacing, height*.7 +chorusValues[i], 12, 12);
 	}
+}
+
+function exciteSinger(){
+	singerAmplitude = 70;
+	singerTheta = 0.10;
+}
+function calmSinger(){
+	singerAmplitude = 20;
+	singerTheta = 0.02;
+}
+function exciteChorus(){
+	chorusAmplitude = 70;
+	chorusTheta = 0.10;
+}
+function calmChorus(){
+	chorusAmplitude = 20;
+	chorusTheta = 0.02;
 }
